4d2321e2a6386816cdd2bff5b0d23d2aa33e5140
Rogerio <contaprogramacao230@gmail.com>
2025-04-17 08:52:46 -0300

    terceiro commit

---
+++ projeto3/todos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from todos.models import Todo
+from django.urls import reverse_lazy
+# Create your views here.
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
